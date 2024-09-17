688bb1d793945c290d4852f57eba332a91425824
Yaseen fehad <yaseenfehad@gmail.com>
2024-09-17 09:55:39 +0100

    rerendering

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,12 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 const config = {};
 if (ENV === "production") {
