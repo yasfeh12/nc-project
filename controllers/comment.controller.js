3ff254a65c24db1cf697d9a12b4ddef56142f5c8
Yaseen fehad <yaseenfehad@gmail.com>
2024-09-19 10:14:18 +0100

    patch comment change

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -1,4 +1,7 @@
-const { removeCommentById } = require('../models/comments.model');
+const {
+  removeCommentById,
+  updateCommentById,
+} = require("../models/comments.modell");
 
 exports.deleteCommentById = (req, res, next) => {
   const { comment_id } = req.params;
@@ -6,13 +9,38 @@ exports.deleteCommentById = (req, res, next) => {
   removeCommentById(comment_id)
     .then((deleted) => {
       if (!deleted) {
-        return res.status(404).send({ msg: 'Comment not found' });
+        return res.status(404).send({ msg: "Comment not found" });
       }
-      res.status(204).send(); 
+      res.status(204).send();
     })
     .catch((err) => {
-      if (err.code === '22P02') {
-        res.status(400).send({ msg: 'Invalid comment ID format' });
+      if (err.code === "22P02") {
+        res.status(400).send({ msg: "Invalid comment ID format" });
+      } else {
+        next(err);
+      }
+    });
+};
+
+exports.updateCommentVotes = (req, res, next) => {
+  const { comment_id } = req.params;
+  const { inc_votes } = req.body;
+
+  if (typeof inc_votes !== "number") {
+    return res
+      .status(400)
+      .send({ msg: "Bad request: Missing or invalid field(s)" });
+  }
+
+  updateCommentById(comment_id, inc_votes)
+    .then((updatedComment) => {
+      res.status(200).send({ comment: updatedComment });
+    })
+    .catch((err) => {
+      if (err.code === "22P02" || err.code === "22003") {
+        res.status(400).send({ msg: "Invalid comment ID" });
+      } else if (err.status === 404) {
+        res.status(404).send({ msg: "Comment not found" });
       } else {
         next(err);
       }
